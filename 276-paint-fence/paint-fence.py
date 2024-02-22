1ed9ff6f4425d094d143872b06ccd4b3555ac3ea
Yskak <57975834+yskaaks@users.noreply.github.com>
2024-02-22 16:21:20 +1100

    Time: 48 ms (6.88%) | Memory: 16.5 MB (92.12%) - LeetSync

--- 276-paint-fence/paint-fence.py
+++ 276-paint-fence/paint-fence.py
@@ -1,13 +1,24 @@
 class Solution:
     def numWays(self, n: int, k: int) -> int:
-        memo = {}
-        def dfs(i):
-            if i == 1:
-                return k
-            if i == 2:
-                return k * k
-            if i in memo:
-                return memo[i]
-            memo[i] = (k-1) * (dfs(i-1) + dfs(i-2))
-            return memo[i]
-        return dfs(n)
+        if n == 1:
+            return k
+        if n == 2:
+            return k * k
+        dp = [0] * (n+1)
+        dp[1] = k
+        dp[2] = k*k
+        for i in range(3, n+1):
+            dp[i] = (k-1) * (dp[i-1] + dp[i-2])
+        return dp[n]
+
+        # memo = {}
+        # def dfs(i):
+        #     if i == 1:
+        #         return k
+        #     if i == 2:
+        #         return k * k
+        #     if i in memo:
+        #         return memo[i]
+        #     memo[i] = (k-1) * (dfs(i-1) + dfs(i-2))
+        #     return memo[i]
+        # return dfs(n)
