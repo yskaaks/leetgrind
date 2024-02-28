925f16074bc8890ce009463a539341186a8ac8b5
Yskak <57975834+yskaaks@users.noreply.github.com>
2024-02-28 12:27:25 +1100

    Time: 176 ms (76.18%) | Memory: 20 MB (61.76%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -1,28 +1,53 @@
+class ListNode:
+    def __init__(self, key, value):
+        self.pair = (key, value)
+        self.next = None
+
 class MyHashMap:
 
     def __init__(self):
-        self.keys = []
-        self.vals = []
+        self.size = 1000
+        self.buckets = [None] * self.size
 
     def put(self, key: int, value: int) -> None:
-        if key not in self.keys:
-            self.keys.append(key)
-            self.vals.append(value)
+        index = key % self.size
+
+        if not self.buckets[index]:
+            self.buckets[index] = ListNode(key, value)
+            return
         else:
-            i = self.keys.index(key)
-            self.vals[i] = value
+            curr = self.buckets[index]
+            while True:
+                if curr.pair[0] == key:
+                    curr.pair = (key, value)
+                    return
+                if not curr.next:
+                    break
+                curr = curr.next
+            curr.next = ListNode(key,value)
 
     def get(self, key: int) -> int:
-        if key not in self.keys:
-            return -1
-        i = self.keys.index(key)
-        return self.vals[i]
+        index = key % self.size
+        curr = self.buckets[index]
+        while curr:
+            if curr.pair[0] == key:
+                return curr.pair[1]
+            curr = curr.next
+        return -1
 
     def remove(self, key: int) -> None:
-        if key in self.keys:
-            i = self.keys.index(key)
-            self.vals.pop(i)
-            self.keys.remove(key)
+        index = key % self.size
+        curr = self.buckets[index]
+        prev = None
+        while curr:
+            if curr.pair[0] == key:
+                if prev:
+                    prev.next = curr.next
+                else:
+                    self.buckets[index] = curr.next
+                return
+            prev = curr
+            curr = curr.next
         
 
 # Your MyHashMap object will be instantiated and called as such:
