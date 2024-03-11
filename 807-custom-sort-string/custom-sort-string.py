c363d99d03e4a4aab19d70860943d05a9aa83000
Yskak <57975834+yskaaks@users.noreply.github.com>
2024-03-11 11:34:47 +1100

    Time: 31 ms (81.57%) | Memory: 16.8 MB (16.02%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -5,17 +5,18 @@ class Solution:
         res = []
         for char in order:
             if char in s_map:
-                res.append(char)
-                s_map[char] -= 1
-                if s_map[char] != 0:
-                    for i in range(s_map[char]):
-                        res.append(char)
-                        s_map[char] -= 1
-            if s_map[char] == 0:
+                res.extend(char * s_map[char])
+            #     s_map[char] -= 1
+            #     if s_map[char] != 0:
+            #         for i in range(s_map[char]):
+            #             res.append(char)
+            #             s_map[char] -= 1
+            # if s_map[char] == 0:
                 del s_map[char]
         
-        for char in s_map:
-            for i in range(s_map[char]):
-                res.append(char)
-                s_map[char] -= 1
+        for char, count in s_map.items():
+            res.extend(char * count)
+            # for i in range(s_map[char]):
+            #     res.append(char)
+            #     s_map[char] -= 1
         return "".join(res)
