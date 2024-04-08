86a8aef2f5f45490ed0dcd33e523748caf63e6c9
Yskak <57975834+yskaaks@users.noreply.github.com>
2024-04-08 17:39:43 +1000

    Time: 39 ms (63.55%) | Memory: 16.6 MB (59.90%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        
+        circle_student_count, square_student_count = 0, 0
+
+        for student in students:
+            if student == 0:
+                circle_student_count += 1
+            else:
+                square_student_count += 1
+        for sandwich in sandwiches:
+
+            if sandwich == 0 and circle_student_count == 0:
+                return square_student_count
+            if sandwich == 1 and square_student_count == 0:
+                return circle_student_count
+            
+            if sandwich == 0:
+                circle_student_count -= 1
+            else:
+                square_student_count -= 1
+        return 0
