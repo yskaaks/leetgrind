d1a9acc32ed36b60d2acf268cddae19d25488bbc
Yskak <57975834+yskaaks@users.noreply.github.com>
2024-04-24 11:57:18 +1000

    Time: 41 ms (13.84%) | Memory: 16.5 MB (85.24%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n < 3:
+            return 1 if n else 0
+        dp = [0] * (n+1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        return dp[-1]
